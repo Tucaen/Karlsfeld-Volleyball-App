0dc3d88b5bacc1684c874befbed7124fc8ff7b31
Tucaen <tobias.winbauer@yahoo.de>
2015-07-18 09:08:32 +0200

    Testing if version control is working now. Added DB_Communicator to interact with the MySQL database.

--- VolleyballApp/MainActivity.cs
+++ VolleyballApp/MainActivity.cs
@@ -7,16 +7,12 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
-using Microsoft.OneDrive.Pickers;
-
 namespace VolleyballApp
 {
 	[Activity (Label = "VolleyballApp", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
 		int count = 1;
-		private IPicker picker;
-		static string ONEDRIVE_APP_ID = "000000004015DC12";
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -25,16 +21,9 @@ namespace VolleyballApp
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			//Create a picker
-			picker = Picker.CreatePicker(ONEDRIVE_APP_ID);
-
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button btnPicker = FindViewById<Button> (Resource.Id.OneDrivePicker);
-			
-			btnPicker.Click += delegate {
-				picker.StartPicking(this, LinkType.DownloadLink);
-			};
+			DB_Communicator db = new DB_Communicator();
+			db.test();
+		
 		}
 	}
 }
