2a26f2dc3666ecac9bf4d8494e7dca7b57ae6a1d
Tucaen <tobias.winbauer@yahoo.de>
2015-07-18 17:29:26 +0200

    Testing..This version controll is behaving strange

--- VolleyballApp/SQL/Insert_Event.sql
+++ VolleyballApp/SQL/Insert_Event.sql
@@ -27,3 +27,4 @@ INSERT INTO `volleyball_app_db`.`event`
 (`idEvent`,`Name`,`StartDate`,`EndDate`,`Location`)
 VALUES
 (4,'Training','2015-08-05 19:30:00','2015-08-05 21:30:00','Dachau - Berufsschulhalle');
+
