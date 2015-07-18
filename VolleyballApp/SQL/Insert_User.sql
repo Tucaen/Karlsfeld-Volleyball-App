2bf506609c9fa74f69414e01e83a48172cd68789
Tucaen <tobias.winbauer@yahoo.de>
2015-07-18 14:43:37 +0200

    Finally added the f*cking SQL-Scripts

---
+++ VolleyballApp/SQL/Insert_User.sql
@@ -0,0 +1,35 @@
+INSERT INTO `volleyball_app_db`.`user`
+(`idUser`,`Name`,`Role`,`Password`,`Number`,`Position`)
+VALUES
+(1,'Tobias Winbauer','Spieler','',13,'Mitte');
+
+INSERT INTO `volleyball_app_db`.`user`
+(`idUser`,`Name`,`Role`,`Password`,`Number`,`Position`)
+VALUES
+(2,'Marcel Winbauer','Spieler','',10,'Aussen');
+
+INSERT INTO `volleyball_app_db`.`user`
+(`idUser`,`Name`,`Role`,`Password`,`Number`,`Position`)
+VALUES
+(3,'Luis Dick','Spieler','',37,'Diagonal');
+
+INSERT INTO `volleyball_app_db`.`user`
+(`idUser`,`Name`,`Role`,`Password`,`Number`,`Position`)
+VALUES
+(4,'Mathias Langer','Spieler','',7,'Steller');
+
+INSERT INTO `volleyball_app_db`.`user`
+(`idUser`,`Name`,`Role`,`Password`,`Number`,`Position`)
+VALUES
+(5,'Thomas Fluck','Spieler','',18,'Aussen');
+
+INSERT INTO `volleyball_app_db`.`user`
+(`idUser`,`Name`,`Role`,`Password`,`Number`,`Position`)
+VALUES
+(6,'Felix Hoffmann','Spieler','',20,'Mitte');
+
+
+INSERT INTO `volleyball_app_db`.`user`
+(`idUser`,`Name`,`Role`,`Password`,`Number`,`Position`)
+VALUES
+(7,'Lukas Hoffmann','Spieler','',0,'?');
